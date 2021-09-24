927f5690fbeb74930f8a4cc98bf82b92468c9acf
Anton <rozum.ant@gmail.com>
2021-09-24 13:23:07 +0300

    Initial commit

---
+++ LongValue.h
@@ -0,0 +1,22 @@
+//
+// Created by Anton on 22/09/2021.
+//
+
+#ifndef LAB2_LONGVALUE_H
+#define LAB2_LONGVALUE_H
+
+
+class LongValue
+{
+public:
+	LongValue(string value)
+	{
+
+	}
+
+private:
+	int[] value;
+};
+
+
+#endif //LAB2_LONGVALUE_H
