0310bfa522e9d722b29e4eb465b00a68a0cdf772
Anton <rozum.ant@gmail.com>
2021-10-15 18:32:49 +0300

    WIP

--- LongValue.h
+++ LongValue.h
@@ -5,17 +5,180 @@
 #ifndef LAB2_LONGVALUE_H
 #define LAB2_LONGVALUE_H
 
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+class Multiplication;
 
 class LongValue
 {
 public:
-	LongValue(string value)
+	static Multiplication *multiplication;
+	string digits;
+
+	LongValue(string value = "")
+	{
+		digits = value;
+	}
+
+	LongValue operator*(LongValue &other);
+
+	LongValue operator+(LongValue other);
+
+	LongValue operator=(string value);
+
+	LongValue operator=(int value);
+
+private:
+	void AddValue(int &remainder, string &newValue, int &value)
+	{
+		if(remainder != 0)
+		{
+			value += remainder;
+			remainder = 0;
+		}
+		if(value >= 10)
+		{
+			value %= 10;
+			remainder += 1;
+		}
+		newValue += value + '0';
+	}
+
+	void SumValue(int &remainder, string &newValue, char value, char otherValue)
+	{
+		int num = otherValue - '0' + value - '0';
+		if(remainder != 0)
+		{
+			num += remainder;
+			remainder = 0;
+		}
+		if(num >= 10)
+		{
+			num %= 10;
+			remainder += 1;
+		}
+		newValue += num + '0';
+	}
+
+	LongValue CalculateValues(string longerValue, string shorterValue)
+	{
+		int remainder = 0;
+		string newValue = "";
+		string otherValue = "";
+		int digitsDifference = longerValue.length() - shorterValue.length();
+		for(int i = 0; i < digitsDifference; i++)
+		{
+			otherValue += "0";
+		}
+		otherValue += shorterValue;
+		for(int i = longerValue.length() - 1; i >= digitsDifference; i--)
+		{
+			SumValue(remainder, newValue, longerValue[i], otherValue[i]);
+		}
+		for(int i = digitsDifference - 1; i >= 0; i--)
+		{
+			int value = longerValue[i] - '0';
+			AddValue(remainder, newValue, value);
+		}
+		reverse(newValue.begin(), newValue.end());
+		LongValue value(newValue);
+		return value;
+	}
+};
+
+istream &operator>>(istream &is, LongValue &l)
+{
+	is >> l.digits;
+	return is;
+}
+
+ostream &operator<<(ostream &os, LongValue &l)
+{
+	os << l.digits;
+	return os;
+}
+
+class Multiplication
+{
+public:
+	virtual LongValue Multiply(LongValue a, LongValue b) = 0;
+};
+
+LongValue LongValue::operator*(LongValue &other)
+{
+	return multiplication->Multiply(*this, other);
+}
+
+LongValue LongValue::operator=(string value)
+{
+	digits = value;
+	return *this;
+}
+
+LongValue LongValue::operator=(int value)
+{
+	digits = to_string(value);
+	return *this;
+}
+
+LongValue LongValue::operator+(LongValue other)
+{
+	if(digits.length() > other.digits.length())
+	{
+		return CalculateValues(digits, other.digits);
+	}
+	else if(other.digits.length() > digits.length())
+	{
+		return CalculateValues(other.digits, digits);
+	}
+	else
+	{
+		int remainder = 0;
+		string newValue = "";
+		for(int i = digits.length() - 1; i >= 0; i--)
+		{
+			SumValue(remainder, newValue, digits[i], other.digits[i]);
+		}
+		if(remainder != 0)
+		{
+			newValue += remainder + '0';
+		}
+		reverse(newValue.begin(), newValue.end());
+		LongValue value(newValue);
+		return value;
+	}
+}
+
+class KaratsubaMultiplication : public Multiplication
+{
+public:
+	LongValue Multiply(LongValue a, LongValue b) override
+	{
+		int adigits = a.digits.length();
+		int bdigits = b.digits.length();
+		int n = adigits % 2 == 0 ? adigits : adigits + 1;
+		int m = bdigits % 2 == 0 ? bdigits : bdigits + 1;
+
+		LongValue l("45");
+		return l;
+	}
+};
+
+class NaiveMultiplication : public Multiplication
+{
+public:
+	LongValue Multiply(LongValue a, LongValue b) override
 	{
 
 	}
 
-private:
-	int[] value;
+	LongValue Multiply(LongValue a, int b)
+	{
+
+	}
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-#include <iostream>
+#include "LongValue.h"
+
+Multiplication *LongValue::multiplication = new KaratsubaMultiplication();
 
 int main()
 {
+	LongValue a, b, c;
+	a = 281, b = 3679, c = 45;
+
+	c = a + b;
+	cout << a << " " << b << " " << c;
+	//a = c * b;
+	//a.print();
+	return 0;
 }
