c1876d85d194afa037fe094c09ecf4caed04cb17
Anton <rozum.ant@gmail.com>
2021-11-04 17:30:18 +0200

    Demonstrating Toom-Cook multiplication

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "LongValue.h"
 
-Multiplication *LongValue::multiplication = new KaratsubaMultiplication();
+Multiplication *LongValue::multiplication = new ToomCookMultiplication();
+Multiplication *LongValue::basicMultiplication = new BasicMultiplication();
 
 
 int main()
 {
 	LongValue a, b, c;
 
-	a = "4368948378334566345674";
-	b = "5564590345634458436435";
+	a = "1234567890123456789012425725346346246432";
+	b = "987654321987654321098435643745375463426432";
 	c = a * b;
 	cout << c << endl;
 	return 0;
