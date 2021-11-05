3ac83594913fbc728ad068c5761651b594448575
Anton <rozum.ant@gmail.com>
2021-11-05 13:26:16 +0200

    Lehmer test updated

--- LehmerPrimeTest.cpp
+++ LehmerPrimeTest.cpp
@@ -3,11 +3,26 @@
 //
 
 #include "LehmerPrimeTest.h"
-#include "LongValue.h"
 
 bool LehmerPrimeTest::IsPrime(LongValue value)
 {
-	LongValue zero("0");
+	LongValue one("1");
+	for(size_t i = 0; i < 10; i++)
+	{
+		LongValue a = GetRandom(value);
+
+		LongValue p = (value - one) / 2;
+
+		LongValue pw = pow(a, p);
+		LongValue md = pw % value;
+
+		LongValue res = value - md;
+		if(md != one && res != one)
+			return false;
+	}
+	return true;
+
+	/*LongValue zero("0");
 	LongValue one("1");
 	LongValue two("2");
 	LongValue S("4");
@@ -26,5 +41,5 @@ bool LehmerPrimeTest::IsPrime(LongValue value)
 		S = test1 % M;
 		k = k + one;
 	}
-	return S == zero;
+	return S == zero;*/
 }
